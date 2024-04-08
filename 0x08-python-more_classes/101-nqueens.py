7bcb3bfb81717aede5d72911d3bfab434dc84505
mariamihabmo <mariam.ihab.mo@gmail.com>
2024-04-08 21:16:54 +0200

    nn

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -139,4 +139,4 @@ if __name__ == "__main__":
     board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
-        print(sol)
+        print(*sol, sep='\n')
